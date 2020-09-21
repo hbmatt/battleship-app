b1ffc860b1535ae1e6023b517c9fc88555e20c8f
HBMATT <HANNAHBMATT@GMAIL.COM>
2020-09-21 18:39:16 -0500

    Updates

---
+++ precache-manifest.7f05d7c157b33d72b1e8be8b75c31fb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b0f099a15cd3ad82d8da1c4196817fa",
+    "url": "/battleship-app/index.html"
+  },
+  {
+    "revision": "3ff82b86abfd35994081",
+    "url": "/battleship-app/static/css/main.fcfbecd9.chunk.css"
+  },
+  {
+    "revision": "b09c62162fe93e01f33e",
+    "url": "/battleship-app/static/js/2.9de319af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship-app/static/js/2.9de319af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff82b86abfd35994081",
+    "url": "/battleship-app/static/js/main.e6d68478.chunk.js"
+  },
+  {
+    "revision": "47d4553c46bee85c02ca",
+    "url": "/battleship-app/static/js/runtime-main.81acb71d.js"
+  }
+]);
